f7b207f323c0e0ba663b56d595171b6cc8f88d4a
Romain Boisselle <romain@boisselle.dev>
2019-11-05 12:06:20 +0100

    fix kapt usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,46 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
-    repositories {
-        google()
-        jcenter()
-    }
-    dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.3.50"))
-    }
+  repositories {
+    google()
+    jcenter()
+  }
+  dependencies {
+    classpath(kotlin("gradle-plugin", version = "1.3.50"))
+  }
 }
 
 plugins {
-    kotlin("jvm") version "1.3.50"
+  kotlin("jvm") version "1.3.50"
+  kotlin("kapt") version "1.3.50"
 }
 
 repositories {
-    google()
-    jcenter()
-    mavenCentral()
+  google()
+  jcenter()
+  mavenCentral()
 }
+
+// FIX KAPT incapacity to find the right variant
+// https://youtrack.jetbrains.com/issue/KT-31641
+val usage = Attribute.of("org.gradle.usage", Usage::class.java)
+
+dependencies {
+  attributesSchema {
+    attribute(usage)
+  }
+}
+
+configurations.all {
+    afterEvaluate {
+        if (isCanBeResolved) {
+            attributes {
+                attribute(usage, project.objects.named(Usage::class.java, "java-runtime"))
+            }
+        }
+    }
+}
+// END FIX KAPT incapacity to find the right variant
 
 dependencies {
     implementation(kotlin("stdlib"))
@@ -26,7 +48,7 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
 }
